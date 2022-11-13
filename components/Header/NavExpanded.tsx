ddfc307f2c6b50a2b656ba3b68d21864f9485e19
cjdomacena <cjdomacena@yahoo.com>
2022-11-12 23:48:43 -0500

    Code Structure changes

--- components/Header/NavExpanded.tsx
+++ components/Header/NavExpanded.tsx
@@ -1,41 +1,67 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { MOVIE_GENRES, TV_GENRES } from "../../lib/constants/genres";
 
-import NavLinks from "./NavLinks";
-type Props = {
-  title: string;
-  items: { id: number; name: string }[];
-  href: string;
-};
-const NavExpanded = ({ title, items, href }: Props) => {
+import NavLink from "./NavLink";
+
+const NavExpanded = () => {
   return (
     <NavigationMenu.Root
-      className="relative transition-all "
+      className="relative transition-all text-xs"
       orientation="horizontal"
     >
-      <NavigationMenu.List className="flex items-center">
-        <NavigationMenu.Item>
+      <NavigationMenu.List className="flex items-center gap-2">
+        <NavigationMenu.Item className="relative">
           <NavigationMenu.Trigger className="uppercase">
-            <NavLinks href={href}>{title}</NavLinks>
+            <NavLink href="/movies">Movies</NavLink>
           </NavigationMenu.Trigger>
           <NavigationMenu.Content
             className="NavigationMenuContent 
-          absolute top-12 left-0 bg-[#1c1c1e] w-auto h-auto rounded shadow-xl shadow-neutral-900 flex gap-4"
+          absolute top-12 left-0 bg-main w-auto h-auto rounded shadow-xl shadow-neutral-900 flex gap-4"
           >
             <div className="p-4 relative">
               <ul className="text-xs text-neutral-300 font-medium whitespace-nowrap grid grid-flow-col grid-rows-4">
-                {items.map((genre) => (
+                {MOVIE_GENRES.map((genre) => (
                   <li key={genre.id}>
-                    <NavLinks
+                    <NavLink
                       href={`/${genre.name.split(" ").join("-").toLowerCase()}`}
                     >
                       {genre.name}
-                    </NavLinks>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
               <div className="absolute -top-2 left-4 border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent border-b-[12px] border-b-[#1c1c1e]  z-10"></div>
             </div>
           </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item className="relative">
+          <NavigationMenu.Trigger className="uppercase">
+            <NavLink href="/tv-shows">TV Shows</NavLink>
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content
+            className="NavigationMenuContent 
+          absolute top-12 left-0 bg-main w-auto h-auto rounded shadow-xl shadow-neutral-900 flex gap-4"
+          >
+            <div className="p-4 relative">
+              <ul className="text-xs text-neutral-300 font-medium whitespace-nowrap grid grid-flow-col grid-rows-4">
+                {TV_GENRES.map((genre) => (
+                  <li key={genre.id}>
+                    <NavLink
+                      href={`/${genre.name.split(" ").join("-").toLowerCase()}`}
+                    >
+                      {genre.name}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+              <div className="absolute -top-2 left-4 border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent border-b-[12px] border-b-[#1c1c1e]  z-10"></div>
+            </div>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item className="text-sm">
+          <NavLink href="/discussion">Discussion</NavLink>
         </NavigationMenu.Item>
       </NavigationMenu.List>
     </NavigationMenu.Root>
--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -1,47 +1,29 @@
-import { IoSearchOutline, IoNotificationsSharp, IoApps } from "react-icons/io5";
+import { IoSearchOutline, IoSunny } from "react-icons/io5";
 import NavExpanded from "./NavExpanded";
-import { MOVIE_GENRES, TV_GENRES } from "../../lib/constants/genres";
-import NavLinks from "./NavLinks";
+import Link from "next/link";
+import UserNav from "./UserNav";
+import Search from "../Inputs/Search";
 
 const Navbar = () => {
   return (
     <header className="p-2 border-b border-b-neutral-800">
-      <nav className=" mx-auto p-4 flex justify-between items-center">
+      <nav className=" mx-auto p-2 flex justify-between items-center">
         <div className="flex gap-12 items-center">
-          <h4 className="text-xl uppercase tracking-wider font-black text-white ">
-            WhatsNext
-          </h4>
-          <ul className="flex gap-2 items-center text-xs uppercase font-semibold tracking-wider">
-            <li>
-              <NavExpanded items={MOVIE_GENRES} href="/movie" title="movies" />
-            </li>
-
-            <li>
-              <NavExpanded
-                items={TV_GENRES}
-                href="/tv-shows"
-                title="TV SHOWS"
-              />
-            </li>
-            <li>
-              <NavLinks href="/animations">Discussions</NavLinks>
-            </li>
-          </ul>
+          <Link href="/">
+            <h4 className="text-xl uppercase tracking-wider font-black text-white ">
+              WhatsNext
+            </h4>
+          </Link>
+          <NavExpanded />
         </div>
-        <div className="flex gap-4 text-neutral-300">
-          <button className="hover:text-neutral-100">
-            <IoSearchOutline className="w-5 h-5" />
-          </button>
-          <button className="hover:text-neutral-100">
-            <IoNotificationsSharp className="w-5 h-5" />
-          </button>
-          <button className="hover:text-neutral-100">
-            <IoApps className="w-5 h-5" />
-          </button>
 
-          <button className="text-sm text-amber-500 font-bold">Log In</button>
-          <div className="border-r border-neutral-500" />
-          <button className="text-sm">Register</button>
+        <div className="flex gap-4 text-neutral-300 items-center">
+          <Search />
+          <button className="hover:text-neutral-100">
+            <IoSunny className="w-5 h-5" />
+          </button>
+          <div className="h-full py-4 border-r border-r-neutral-700 rounded" />
+          <UserNav />
         </div>
       </nav>
     </header>
