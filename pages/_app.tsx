cae8f5f898f52c9f0152cabe378d6c58e92a5372
cjdomacena <cjdomacena@yahoo.com>
2022-11-12 23:48:09 -0500

    Initialize supabase client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,26 @@ import "@fontsource/montserrat/800.css";
 import "@fontsource/montserrat/900.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Header/Navbar";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import Toggle from "../components/Buttons/Toggle";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <main>
-      <Navbar />
-      <Component {...pageProps} />
-    </main>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <main>
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+    </SessionContextProvider>
   );
 }
