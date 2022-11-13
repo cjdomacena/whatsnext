82fa7b2a11bed4921fa04f93313af69884448a82
cjdomacena <cjdomacena@yahoo.com>
2022-11-12 23:47:46 -0500

    Styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#1c1c1e",
+      },
+    },
   },
   plugins: [],
 };
