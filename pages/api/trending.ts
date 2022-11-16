725a63496bbeaaf1e45fac4fc13a6b8156c61bb5
cjdomacena <cjdomacena@yahoo.com>
2022-11-16 14:35:55 -0500

    Implement error handling

---
+++ pages/api/trending.ts
@@ -0,0 +1,42 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
+export enum TimeWindow {
+  "day",
+  "week",
+}
+export enum MediaType {
+  "all",
+  "tv",
+  "movie",
+  "person",
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { query } = req;
+  let time_window = query?.time_window ?? "week";
+  let media_type = query?.media_type ?? "all";
+  try {
+    const data = await fetch(
+      `${process.env.TMDB_URL}/trending/${media_type}/${time_window}?api_key=${process.env.TMDB_API_KEY}`
+    );
+    const results = await data.json();
+    if (results.hasOwnProperty("success") && !results.success) {
+      throw new Error(results.status_message);
+    }
+    return res.status(200).json(results);
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({
+        error: e.message,
+        status: 500,
+      });
+    }
+    return res
+      .status(500)
+      .json({ error: "Something went wrong...", status: 500 });
+  }
+}
