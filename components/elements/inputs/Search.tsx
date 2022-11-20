01eee0cde0c34dc19ecec2a41b1018c67b61c4ed
cjdomacena <cjdomacena@yahoo.com>
2022-11-20 01:46:54 -0500

    wip

--- components/elements/inputs/Search.tsx
+++ components/elements/inputs/Search.tsx
@@ -2,7 +2,7 @@ import { IoSearchOutline } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className="w-72 rounded bg-main flex items-center ring-1 ring-neutral-800  px-2 focus-within:ring-neutral-600 text-sm transition-all font-medium tracking-widest">
+    <div className="w-full flex-grow rounded flex items-center ring-1 ring-neutral-900  px-2 focus-within:ring-neutral-600 text-xs transition-all font-medium tracking-widest">
       <label htmlFor="quick-search">
         <IoSearchOutline className="w-4 h-4 text-neutral-500" />
       </label>
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,17 +1,19 @@
-import NavExpanded from "../elements/header/NavExpanded";
 import Link from "next/link";
 import UserNav from "../elements/header/usernav";
+import Search from "../elements/inputs/Search";
 
 const Navbar = () => {
   return (
     <header className="p-2 border-b border-b-neutral-800 shadow">
-      <nav className=" mx-auto p-2 flex justify-between items-center">
-        <div className="flex gap-12 items-center">
+      <nav className=" mx-auto p-2 flex justify-between items-center gap-2">
+        <div className="flex gap-4 items-center">
           <h4 className="text-xl uppercase tracking-wider font-black text-white ">
             <Link href="/">WhatsNext</Link>
           </h4>
-          <NavExpanded />
+
+          {/* <NavExpanded /> */}
         </div>
+        <Search />
 
         <div className=" gap-4 text-neutral-300 items-center 2xl:flex xl:flex lg:flex hidden">
           <UserNav />
